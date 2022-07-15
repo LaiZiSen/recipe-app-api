ee80563331e5f3f63c5e36db5af71490e163272a
LaiZiSen <samlain060504>
2022-07-15 16:39:45 +0800

    Downloaded dependancies for postgresql and configured django

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subtracct x from y and return resilt."""
+    return 0
